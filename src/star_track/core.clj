d43204ad9a80726f89c67bb270a94b2da3de5f0e
Bahadir Cambel <bcambel@gmail.com>
2015-01-09 15:33:41 +0100

    initial

---
+++ src/star_track/core.clj
@@ -0,0 +1,7 @@
+(ns star-track.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/star_track/core_test.clj
@@ -0,0 +1,7 @@
+(ns star-track.core-test
+  (:require [clojure.test :refer :all]
+            [star-track.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
