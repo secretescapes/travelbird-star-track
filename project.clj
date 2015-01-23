37fe654ebb22a38824a1135c8e195a388e99c3e7
Bahadir Cambel <bcambel@gmail.com>
2015-01-23 01:13:27 +0100

    add kafka dependency and kafka component sys

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [org.clojure/clojure "1.6.0"]
+    [org.clojure/core.memoize "0.5.6"]
+    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
     [com.stuartsierra/component "0.2.2"]
     [compojure "1.3.1"]
     [ring "1.3.2"]
@@ -12,6 +14,7 @@
     [com.taoensso/timbre "3.3.1-1cd4b70"]
     [cheshire "5.4.0"]
     [midje "1.7.0-SNAPSHOT"]
+    [clj-kafka/clj-kafka "0.2.8-0.8.1.1"]
     ]
   :java-agents [[com.newrelic.agent.java/newrelic-agent "2.19.0"]]
   :main star-tracker.core
---
+++ src/star_tracker/system/kafka.clj
@@ -0,0 +1,67 @@
+(ns star-tracker.system.kafka
+  (:use [clj-kafka.core :only (with-resource)])
+  (:require
+    [clj-kafka.zk                           :as zk]
+    [clj-kafka.consumer.zk                  :as consumer.zk]
+    [clj-kafka.producer                     :as kfk.prod]
+    [clojure.core.async                     :as async :refer [alts! go chan]]
+    [com.stuartsierra.component             :as component]
+    [taoensso.timbre                        :as timbre
+           :refer (log  trace  debug  info  warn  error  fatal  report)]))
+
+(defn get-broker-list
+  "Returns a comma separated list of brokers"
+  [zookeeper]
+  (zk/broker-list 
+    (zk/brokers {"zookeeper.connect" zookeeper})))
+
+(defn send!
+  [producer topic msg]
+  (kfk.prod/send-message producer
+              (kfk.prod/message topic (.getBytes msg))))
+
+(defrecord KafkaProducer [zookeeper prod-chan channel]
+  component/Lifecycle
+
+  (start [component]
+    (try
+      (warn "Starting KAFKA PRODUCER Component")
+      (let [producing-channel (or prod-chan (chan))
+            brokers (get-broker-list zookeeper)
+            producer-config {"metadata.broker.list" brokers
+                           "serializer.class" "kafka.serializer.DefaultEncoder"
+                           "partitioner.class" "kafka.producer.DefaultPartitioner"}
+            producer (kfk.prod/producer producer-config)]
+        (go (while true
+          (let [[[topic msg] ch] (alts! [producing-channel])]
+            (send! producer topic msg))))
+         (assoc component :channel producing-channel))
+      (catch Throwable t 
+        (do 
+          (warn "[KAFKA-PROD] FAILED")
+          (error t)
+          (assoc component :channel nil)))))
+  (stop [component]
+
+    )
+  )
+
+(defn kafka-producer
+  [zookeeper]
+  (map->KafkaProducer {:zookeeper zookeeper}))
+
+
+(defrecord KafkaConsumer [zookeeper topics]
+  component/Lifecycle
+
+  (start [component]
+    (let [consumer-config {"zookeeper.connect" zookeeper
+             "group.id" "clj-kafka.consumer"
+             "auto.offset.reset" "smallest"
+             "auto.commit.enable" "true"}]
+
+             )
+    )
+  (stop [component])
+
+  )
