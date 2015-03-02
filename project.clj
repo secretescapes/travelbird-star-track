5adbd9a5b1ed984ee495a31101dc8551816cf33c
Bahadir Cambel <bcambel@gmail.com>
2015-03-02 16:57:49 +0100

    fix nippy versions

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject star-tracker "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject star-tracker "0.1.2"
+  :description "Event collector"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -12,11 +12,12 @@
     [compojure "1.3.1"]
     [ring "1.3.2"]
     [http-kit "2.1.16"]
-    [com.taoensso/timbre "3.3.1-1cd4b70"]
+    [com.taoensso/timbre "3.3.1-1cd4b70" :exclusions [org.clojure/tools.reader]]
     [cheshire "5.4.0"]
     [midje "1.7.0-SNAPSHOT"]
     [clj-kafka/clj-kafka "0.2.8-0.8.1.1"]
-    [amazonica "0.3.18"]
+    [amazonica "0.3.18" :exclusions [com.taoensso/encore org.clojure/tools.reader com.taoensso/nippy]]
+    [com.taoensso/nippy "2.7.0"]
     ]
   :java-agents [[com.newrelic.agent.java/newrelic-agent "2.19.0"]]
   :main star-tracker.core
