92bbd7a37d31d71087b2188b9f4140d7f4c4b746
Bahadir Cambel <bcambel@gmail.com>
2015-03-17 12:06:41 +0100

    no more snapshot midje

--- project.clj
+++ project.clj
@@ -14,7 +14,6 @@
     [http-kit "2.1.16"]
     [com.taoensso/timbre "3.3.1-1cd4b70" :exclusions [org.clojure/tools.reader]]
     [cheshire "5.4.0"]
-    [midje "1.7.0-SNAPSHOT"]
     [clj-kafka/clj-kafka "0.2.8-0.8.1.1"]
     [amazonica "0.3.18" :exclusions [com.taoensso/encore org.clojure/tools.reader com.taoensso/nippy]]
     [com.taoensso/nippy "2.7.0"]
@@ -25,5 +24,7 @@
   :jvm-opts ["-Dlog4j.configuration=file:./log4j.properties"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-      :dev {:plugins [[lein-midje "3.1.3"]]}
+      :dev {
+        :dependencies [[midje "1.6.0" :exclusions [org.clojure/clojure]]]
+        :plugins [[lein-midje "3.1.3"]]}
     })
